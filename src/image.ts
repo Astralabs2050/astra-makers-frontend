9ea5289b71af8a70d0ddd8f482634e701980d782
Tomiwa Bada <tomiwabd@gmail.com>
2024-10-22 17:36:01 +0100

    Add assets

--- src/image.ts
+++ src/image.ts
@@ -8,10 +8,93 @@ const physicalMaker = "/physicalMaker.png";
 const digitalCreator = "/digitalCreator.png";
 const uploadImage = "/upload.svg";
 const selectedIcon = "/selected.svg";
-
+const cancelIcon = "/cancel.svg";
 const arrowLeft = "/arrow-left.svg";
+const addImageIcon = "/add.svg";
+const editIcon = "/astra-edit.svg";
+const deleteIcon = "/astra-delete.svg";
+const astraAddwork = "/astra-addWork.svg";
+const portfolioSample1 = "/portfolioSample1.png";
+const portfolioSample2 = "/portfolioSample2.png";
+const modalImage = "/modalImage.png";
+const linkIcon = "/linkIcon.svg";
+const notificationIcon = "/notification.svg";
+const astraCoin = "/astraCoin.png";
+const minimizeIcon = "/minimize.svg";
+const profilePicture = "/profilePicture.png";
+const dashboardIcon = "/darkDashboardIcon.svg";
+const activeDashboardIcon = "/lightDashboardIcon.svg";
+const earningIcon = "/darkEarningIcon.svg";
+const activeEarningIcon = "/lightEarningIcon.svg";
+const ongoingJobsIcon = "/darkOngoingJobsIcon.svg";
+const activeOngoingJobsIcon = "/lightOngoingJobsIcon.svg";
+const messageIcon = "/darkMessageIcon.svg";
+const activeMessageIcon = "/lightMessageIcon.svg";
+const settingsIcon = "/darkSettingsIcon.svg";
+const activeSettingsIcon = "/lightSettingsIcon.svg";
+const helpAndSupportIcon = "/darkHelpAndSupportIcon.svg";
+const filterIcon = "/filterIcon.svg";
+const heartIcon = "/heartIcon.svg";
+const goBackIcon = "/goBack.svg";
+const sample1 = "/genImage1.png";
+const sample2 = "/genImage2.png";
+const sample3 = "/genImage3.png";
+const sample4 = "/genImage4.png";
+const dropDownIcon = "/dropdown.svg";
+const sendIcon = "/send.svg";
+const workSelected = "/workSelected.svg";
+const webIcon = "/web.svg";
+const confirmIcon = "/confirmIcon.svg";
+const actionIcon = "/menu.svg";
+const earningsIcon = "/earningsIcon.svg";
+const availableIcon = "/availableIcon.svg";
+const completedIcon = "/completedIcon.svg";
+const invoiceIcon = "/invoiceIcon.svg";
+const trendingUp = "/trending-up.svg";
+const logoutIcon = "/logout-icon.svg";
+const profileSettingsIcon = "/profile-settings.svg";
+const notificationSettingsIcon = "/notification-settings.svg";
+const paymentSettingsIcon = "/payment-settings.svg";
+const securitySettingsIcon = "/password-settings.svg";
+const cancelSideIcon = "/cancelSide.svg";
+const cloudIcon = "/cloud.svg";
+const withdrawIcon = "/Subtract.svg";
+const report = "/report.svg";
+const settingsBack = "/back.svg";
+const uploadPicIcon = "/uploadPic.svg";
+const settingsUpload = "/Textfield.svg";
+const verifiedIcon = "/Verify.svg";
 
 export {
+  verifiedIcon,
+  settingsUpload,
+  settingsBack,
+  uploadPicIcon,
+  withdrawIcon,
+  report,
+  cloudIcon,
+  cancelSideIcon,
+  logoutIcon,
+  profileSettingsIcon,
+  notificationSettingsIcon,
+  paymentSettingsIcon,
+  securitySettingsIcon,
+  trendingUp,
+  actionIcon,
+  confirmIcon,
+  webIcon,
+  workSelected,
+  dashboardIcon,
+  activeDashboardIcon,
+  earningIcon,
+  activeEarningIcon,
+  ongoingJobsIcon,
+  activeMessageIcon,
+  activeOngoingJobsIcon,
+  messageIcon,
+  settingsIcon,
+  activeSettingsIcon,
+  helpAndSupportIcon,
   passwordEye,
   googleLogo,
   authRight,
@@ -22,4 +105,30 @@ export {
   digitalCreator,
   uploadImage,
   selectedIcon,
+  cancelIcon,
+  addImageIcon,
+  editIcon,
+  deleteIcon,
+  astraAddwork,
+  portfolioSample1,
+  portfolioSample2,
+  modalImage,
+  linkIcon,
+  notificationIcon,
+  astraCoin,
+  minimizeIcon,
+  profilePicture,
+  filterIcon,
+  heartIcon,
+  goBackIcon,
+  sample1,
+  sample2,
+  sample3,
+  sample4,
+  dropDownIcon,
+  sendIcon,
+  earningsIcon,
+  completedIcon,
+  availableIcon,
+  invoiceIcon,
 };
