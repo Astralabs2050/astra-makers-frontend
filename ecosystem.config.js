611c4d9f6c44eeeaeb9c44181019828c420981bf
Lawblaze <lawblaze4@gmail.com>
2024-11-28 22:08:55 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "my-ts-app",
-      script: "src/index.ts", // Entry point of your TypeScript project
-      interpreter: "node", // Use Node.js as the interpreter
+       name: "next-app",
+      script: "npm",
+      args: "start",
+      interpreter: "none", // Ensures npm is run as a shell command
       interpreter_args: "-r ts-node/register", // Register ts-node to compile TypeScript on the fly
       watch: true, // Optional: Enable file watching
       env: {
