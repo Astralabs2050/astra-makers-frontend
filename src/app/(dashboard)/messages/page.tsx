36c0535e96f08ea28c5c17c24fd84aa1b5fcdebf
Tomiwa Bada <tomiwabd@gmail.com>
2025-03-09 22:46:54 +0100

    fix session storage

--- src/app/(dashboard)/messages/page.tsx
+++ src/app/(dashboard)/messages/page.tsx
@@ -1,5 +1,13 @@
-import MessageFrame from "@/components/messages/MessageFrame";
+"use client";
+
 import DashboardFrame from "@/shared/DashboardFrame";
+import dynamic from "next/dynamic";
+const MessageFrame = dynamic(
+  () => import("@/components/messages/MessageFrame"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Messages() {
   return (
